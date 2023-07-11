7ff90dcfae6540be287f8c561c266414d29c401e
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-07-11 11:43:05 +0100

    Set up the utilities for unit testing

---
+++ TasksLibrary.Tests/Utilities/TaskDbContextMock.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using TasksLibrary.Architecture.Database;
+using TasksLibrary.Models.Interfaces;
+
+namespace TasksLibrary.Tests.Utilities
+{
+    public class TaskDbContextMock : IDbContextMock<DbContext<TaskManagement>>
+    {
+        public Mock<DbContext<TaskManagement>> Build()
+        {
+            var builder = new Mock<DbContext<TaskManagement>>();
+            builder.Setup(s => s.Context.UserRepository).Returns(new Mock<IUserRepository>().Object);
+            builder.Setup(s => s.Context.NoteRepository).Returns(new Mock<INoteRepository>().Object);
+
+            return builder;
+        }
+    }
+}
